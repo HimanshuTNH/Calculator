6ab49f53af95619622d329c2731caa0c77156296
Himanshu Kushwah <146326035+HimanshuTNH@users.noreply.github.com>
2024-05-21 19:45:30 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+ const in1 = document.querySelector(".primary-input");
+ const in2 = document.querySelector(".secondary-input");
+ const plus = document.querySelector(".plus");
+ const minus = document.querySelector(".minus");
+ const into = document.querySelector(".into");
+ const divide = document.querySelector(".divide");
+ const equal = document.querySelector(".equal");
+
+ const a = in1.value;
+ const b = 0;
+ const c = 0;
+
+
